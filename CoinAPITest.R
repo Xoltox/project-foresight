373f9b775544a22f4e8dc1f9f8194a76e98d67a3
Xoltox <parthsharma23055@gmail.com>
2021-04-05 01:10:00 +0530

    Basic Prediction on last 15 days of BTT done

--- CoinAPITest.R
+++ CoinAPITest.R
@@ -2,16 +2,19 @@ library(httr)
 library(jsonlite)
 library(ggplot2)
 library(parsedate)
+library(fUnitRoots)
+library(lmtest)
+library(forecast)
 #library(reshape2)
 
 #Base URL, Do not change
 base_url <- "https://rest.coinapi.io/v1/ohlcv/"
 
 #Data URL
-data_url <- "/history?period_id=1DAY&time_start=2021-03-01&time_end=2021-03-15"
+data_url <- "/history?period_id=1DAY&time_start=2021-01-01"
 
 #Replace with your API Key
-api_key <- "&apikey=#73034021-THIS-IS-SAMPLE-KEY" #73034021-THIS-IS-SAMPLE-KEY
+api_key <- "&apikey=73034021-THIS-IS-SAMPLE-KEY" #73034021-THIS-IS-SAMPLE-KEY
 
 coin <- "BTT/"
 currency <- "INR"
@@ -32,14 +35,36 @@ df$date <- date
 df$price_mean <- (df$price_high + df$price_low) / 2
 df
 
-head(df)
-tail(df)
+#head(df)
+#tail(df)
 summary(df)
-str(df)
+#str(df)
 
-df$type <- "Price (INR)"
+dftrain <- head(df, -15)
 
-ggplot(df, aes(x = date, y = price_mean, ymin = price_low,
-ymax = price_high, fill = type, linetype = type)) +
-geom_line() +
-geom_ribbon(alpha = 0.5)
+tsData = ts(dftrain$price_mean, start = dftrain$date, frequency = 30)
+tsDataActual = ts(df$price_mean, start = df$date, frequency = 30)
+
+plot.ts(tsData)
+
+components.ts = decompose(tsData)
+urkpssTest(tsData, type = c("tau"), lags = c("short"), use.lag = NULL, doplot = TRUE)
+tsstationary = diff(tsData, differences = 1)
+
+components.ts = decompose(tsData)
+urkpssTest(tsDataActual, type = c("tau"), lags = c("short"), use.lag = NULL, doplot = TRUE)
+tsstationaryActual = diff(tsDataActual, differences = 1)
+
+plot(tsstationary)
+
+acf(tsstationary, lag.max = 34)
+pacf(tsstationary, lag.max = 34)
+
+#fitARIMA <- arima(tsstationary, order=c(1,1,1),seasonal = list(order = c(1,0,0), period = 30),method="ML")
+
+fitARIMA <- auto.arima(y = tsstationary, d = 2, D = 1)
+coeftest(fitARIMA)
+
+predict(fitARIMA, n.ahead = 15)
+futurVal <- forecast(fitARIMA, h = 10, level = c(99.5))
+plot(futurVal)
